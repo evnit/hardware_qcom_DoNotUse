c55949118870a970f96d5e372a2056d32776c2e8
Le Anh Dung <evnithn@gmail.com>
2015-04-11 11:18:45 +0700

    Update Android.mk

--- audio-caf/msm8660/legacy/Android.mk
+++ audio-caf/msm8660/legacy/Android.mk
@@ -1,8 +1,5 @@
-ifeq ($(call my-dir),$(call project-path-for,qcom-audio))
-	ifneq ($(filter msm8660,$(TARGET_BOARD_PLATFORM)),)
-		MY_LOCAL_PATH := $(call my-dir)
-		ifeq ($(BOARD_USES_LEGACY_ALSA_AUDIO),true)
-			include $(MY_LOCAL_PATH)/legacy/Android.mk
-		endif
-	endif
+AUDIO_HW_ROOT := $(call my-dir)
+ifeq ($(TARGET_BOARD_PLATFORM),msm8660)
+    include $(AUDIO_HW_ROOT)/msm8660/Android.mk
+    include $(AUDIO_HW_ROOT)/mm-audio/Android.mk
 endif
